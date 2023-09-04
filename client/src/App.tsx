a531f1fe298c26b7b6e8d7c3abc65565185143fd
itsik-ba <112100680+itsik-ba@users.noreply.github.com>
2023-09-04 16:51:15 +0300

    i

---
+++ client/src/App.tsx
@@ -0,0 +1,21 @@
+
+import { Routes } from 'react-router-dom'
+import './App.css'
+import Register from './components/register_login/Register'
+
+function App() {
+  
+ return (
+    <>
+      <div className='main'>
+      <Register />
+      <Routes>
+        
+      </Routes>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/register_login/Register.tsx
@@ -0,0 +1,13 @@
+import "./reg_log.scss";
+import user_Icon from "../../assets/person.png"
+import email_Icon from "../../assets/email.png"
+import password_Icon from "../../assets/password.png"
+
+const Register = () => {
+  return (
+    <div className="register_Main">
+        Register</div>
+  )
+}
+
+export default Register
