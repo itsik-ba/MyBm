40102f65b9e87d6ceb9ca06cc5cf3447a9f2d5f2
itsik-ba <112100680+itsik-ba@users.noreply.github.com>
2023-10-10 19:47:18 +0300

    i

--- backEnd/API/manager/managerControlls.ts
+++ backEnd/API/manager/managerControlls.ts
@@ -41,38 +41,7 @@ const hash  = await bcrypt.hash(userpassword, 10);
 
 export const loginAdmin = async (req:any, res:any) => {
 
-  const { useremail, userpassword } = req.body;
-  
-  connection.query('SELECT useremail, userpassword FROM user WHERE useremail = ?', [useremail], async (error, results) => {
-    if (error) {
-        console.error('Database query error:', error);
-        return res.status(500).json({ message: 'Internal server error' });
-    }
 
-    // Explicitly cast results to User[]
-    const users = results as User[];
 
-    if (users.length > 0) {
-        const user = users[0];
- console.log(user);
-        // Compare the provided email and password with the stored hashed values
-        const emailMatch = await bcrypt.compare(useremail, user.email_hash);
-
-        const passwordMatch = await bcrypt.compare(userpassword, user.password_hash);
-        
-         console.log(emailMatch, useremail, passwordMatch, userpassword);
-        if (emailMatch && passwordMatch) {
-            // Email and password match, authentication successful
-            res.status(200).json({ message: 'Login successful' });
-        } else {
-            // Email and password do not match, authentication failed
-            res.status(401).json({ message: 'Authentication failed' });
-        }
-    } else {
-        // Email not found or results is empty
-        res.status(401).json({ message: 'User not found' });
-    }
-});
-     
   
 }
